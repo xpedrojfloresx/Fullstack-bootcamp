d7c75019ebb238acfd1861968254987efd3f1e80
Pedro Flores <pflores0213@gmail.com>
2025-12-20 13:42:38 -0300

    ultimos detalles integrador

--- Integradores/IntegradorN°1/js/functions.js
+++ Integradores/IntegradorN°1/js/functions.js
@@ -1,7 +1,6 @@
-function menu(){
-    window.location.href = '../index.html';
+function menu() {
+  window.location.href = "../index.html";
 }
-
 
 const popup = document.querySelector("#popup-producto");
 const popupTitulo = document.querySelector("#popup-titulo");
@@ -12,25 +11,24 @@ const btnCerrar = document.querySelector("#btn-cerrar-popup");
 
 const botonesVer = document.querySelectorAll(".btn-more-information");
 
-botonesVer.forEach(boton => {
-    boton.addEventListener("click", () => {
-        const card = boton.closest(".div_gamesCards");
+botonesVer.forEach((boton) => {
+  boton.addEventListener("click", () => {
+    const card = boton.closest(".div_gamesCards");
 
-        const nombre = card.getAttribute("data-name");
-        const precio = card.getAttribute("data-price");
-        const imagen = card.getAttribute("data-img");
-        const descripcion = card.getAttribute("data-descrip");
+    const nombre = card.getAttribute("data-name");
+    const precio = card.getAttribute("data-price");
+    const imagen = card.getAttribute("data-img");
+    const descripcion = card.getAttribute("data-descrip");
 
-        popupTitulo.textContent = nombre;
-        popupPrecio.textContent = precio;
-        popupImg.src = imagen;
-        popupDescrip.textContent = descripcion;
+    popupTitulo.textContent = nombre;
+    popupPrecio.textContent = precio;
+    popupImg.src = imagen;
+    popupDescrip.textContent = descripcion;
 
-        popup.showModal();
-    });
+    popup.showModal();
+  });
 });
 
-
 btnCerrar.addEventListener("click", () => {
-    popup.close();
+  popup.close();
 });
