89c26bd8fcac9473a8a18f8282a3f1af4d0e9075
Pedro Flores <pflores0213@gmail.com>
2026-02-16 16:20:18 -0300

    Agregando practico n9

---
+++ Ejercicios/PracticoN9/server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+
+const path = require('path');
+
+const PORT = 3000;
+
+const app = express();
+
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/index', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'about.html'));
+});
+
+app.get('/contact', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'contact.html'));
+});
+
+app.use((req, res, next) => {
+  res.status(404).json({ message: 'Ruta no encontrada' });
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: 'Error interno del servidor' });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
