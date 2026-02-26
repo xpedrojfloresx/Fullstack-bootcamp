a9d1dbc31196935b52cbef3403c3ed74de7d4554
Pedro Flores <pflores0213@gmail.com>
2026-02-25 21:34:21 -0300

    continuando claseo 08

---
+++ Practicas/clase-08/src/database/conexion.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const dbPassword = process.env.MONGO_PASSWORD;
+
+const uri = `mongodb+srv://pedrojflores:${dbPassword}@cluster0.171yqpp.mongodb.net/?appName=Cluster0`;
+
+const clientOptions = {serverApi: {version: '1', strict: true, deprecationErrors: true}};
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(uri, clientOptions);
+        await mongoose.connection.db.admin().command({ ping: 1 });
+        console.log('Base de datos conectada');
+    } catch (error) {
+        console.error('Error al conectar a la base de datos:', error);
+        process.exit(1);
+    }
+};
+
+module.exports = connectDB;
--- Practicas/clase-08/src/index.js
+++ Practicas/clase-08/src/index.js
@@ -2,7 +2,11 @@ require("dotenv").config();
 
 const app = require("./app");
 
+const connectDB = require("./database/conexion");
+
 const PORT = process.env.PORT || 3000;
+
+connectDB();
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
